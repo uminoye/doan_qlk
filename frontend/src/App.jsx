fdecc91ebe55f7fca2ee23a5f679769845233f85
uminoye <minhthipham857@gmail.com>
2026-06-19 19:02:44 +0700

    Đẩy thư mục frontend lên Github

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1 style={{ color: '#176b52' }}>🚀 Đồ Án Quản Lý Kho Đã Lên Web!</h1>
+      <p>Hệ thống CI/CD đang hoạt động tốt. Chuẩn bị code thôi!</p>
+    </div>
+  )
+}
+export default App
