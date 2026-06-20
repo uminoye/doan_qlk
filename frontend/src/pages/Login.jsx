4364f7de54701abfdec95f2a20ab5ecbae1b3b82
uminoye <minhthipham857@gmail.com>
2026-06-20 20:16:45 +0700

    Sua link API sang Render

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://doan-qlk.onrender.com/api/auth/login', {
+      const response = await fetch('https://doan-qlk.onrender.com/api/auth/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
