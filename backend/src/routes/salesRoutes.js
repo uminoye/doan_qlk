2904feba0cde201e6c11b806ace9b919c46f09af
uminoye <minhthipham857@gmail.com>
2026-06-20 19:13:31 +0700

    Cập nhật backend và ẩn node_modules, .env

--- backend/src/routes/salesRoutes.js
+++ backend/src/routes/salesRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const salesController = require('../controllers/salesController');
+// Nhập khẩu camera an ninh vào đây
 const { verifyManager } = require('../middlewares/authMiddleware');
 
-router.post('/create', salesController.createSalesOrder);
+// Chỉ có ai có thẻ bài hợp lệ và có chức danh MANAGER/ADMIN mới được duyệt đơn
 router.put('/approve/:id', verifyManager, salesController.approveSalesOrder);
 
 module.exports = router;
