2f01a293a9dfe0ccc82770ac279c2fbe07223993
uminoye <minhthipham857@gmail.com>
2026-06-20 20:11:12 +0700

    Sua link API sang Render

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+router.post('/login', authController.login); // Đúng đường dẫn là /api/auth/login
 router.post('/register', authController.register);
-router.post('/login', authController.login);
 
 module.exports = router;
